6b8de45bada7cb098181887966f5253ab9c93964
Sean O'Brien <seancanobrien@hotmail.co.uk>
2024-12-02 18:17:13 +0000

    Change syncing
    Filter files are synced from remote to a temp dir first (this just works
    better). Then we work from this local copy. Then summaries are stores
    locally, then the summaries are copied to remote.

--- make_arxiv_weekly_summary.sh
+++ make_arxiv_weekly_summary.sh
@@ -2,12 +2,22 @@
 
 # Takes in a filter file with an email at the top. Makes the relevant weekly update, then sends that email the weekly update
 
+# There are remote and local copies of the filter files
+# and the stored summaries. This is because using rclone directly
+# on a remote seems unreliable
+remote_dir="google_drive:arxiv_summaries_per_email"
+local_dir="/home/sean/.local/arxiv_weekly_summary/data_local_copy"
+
+local_summary_store="$local_dir/summaries"
+
 construct_summary_and_send_email() {
-  store_dir="/home/sean/google_drive/arxiv_summaries_per_email/"
+  # Read the first line of the supplied file
+  # This is the relavant email address
   read -r send_email < "$1"
+
   echo "sending update email to $send_email"
 
-  specific_email_store_dir="$store_dir$send_email"
+  specific_email_store_dir="$local_summary_store/$send_email"
   echo "storing update html in $specific_email_store_dir"
 
   if [[ ! -d $specific_email_store_dir ]]
@@ -21,24 +31,36 @@ construct_summary_and_send_email() {
   if [[ -n $update_html && -f $update_html && -s $update_html ]]
   then
     echo "generated file $update_html"
-    mutt -e 'set content_type=text/html' -s 'Weekly Arxiv Update' $send_email < $update_html
+    # mutt -e 'set content_type=text/html' -s 'Weekly Arxiv Update' $send_email < $update_html
     echo "sent email containing $update_html to $send_email"
   else
     echo "some error with generated file: $update_html"
   fi
 }
 
-filter_dir="/home/sean/google_drive/arxiv_summaries_per_email/filters/"
+echo "-------------------------------------"
+echo "syncing filters from remote"
+# now upload the local update files to remote
+# Copy over the filter files, enuring they are synchronised with remote filter files
+rclone sync "$remote_dir/filters" "$local_dir/filters"
 
-for filter_file in "$filter_dir"*
+for local_filter_file in "$local_dir/filters/"*
 do
-  if [[ -f $filter_file && -s $filter_file ]]
+  if [[ -f $local_filter_file && -s $local_filter_file ]]
   then
     echo "-------------------------------------"
-    echo "collected filter file $filter_file"
-    construct_summary_and_send_email "$filter_file"
+    echo "collected filter file $local_filter_file"
+    # fix potential dos carriage returns
+    dos2unix $local_filter_file
+    sed -i 's/\r/\n/g' $local_filter_file
+    construct_summary_and_send_email $local_filter_file
   else
-    echo "$filter_file is not a file or is empty, skipping"
+    echo "$local_filter_file is not a file or is empty, skipping"
   fi
 done
 
+echo "-------------------------------------"
+echo "copying summaries to remote"
+# now upload the local update files to remote
+rclone copy $local_summary_store "$remote_dir/summaries/"
+echo "-------------------------------------"
